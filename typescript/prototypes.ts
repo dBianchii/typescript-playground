ca713191f89544bce5fb1f8ce7f1e876d609362e
Gabriel Bianchi <gdbianchii@gmail.com>
2024-02-12 23:04:55 -0300

    asd

---
+++ typescript/prototypes.ts
@@ -0,0 +1,32 @@
+const arr = [1, 2, 3];
+
+arr.length;
+
+arr.join();
+
+arr.map((x) => x * 2); // Creates a new array.
+const newArr = arr.filter((x) => x > 1); // Creates a new array.
+arr.forEach((x) => console.log(x)); // Does not create a new array.
+
+//arr.prototype.concat
+//arr.prototype.filter
+//arr.prototype.map
+//arr.prototype.reduce
+//arr.prototype.reduceRight
+//arr.prototype.reverse
+//arr.prototype.slice
+//arr.prototype.sort
+//arr.prototype.splice
+//arr.prototype.indexOf
+//arr.prototype.lastIndexOf
+//arr.prototype.every
+//arr.prototype.some
+//arr.prototype.forEach
+//arr.prototype.entries
+//arr.prototype.keys
+//arr.prototype.values
+//arr.prototype.find
+//arr.prototype.findIndex
+//arr.prototype.includes
+//arr.prototype.fill
+//arr.prototype.copyWithin
